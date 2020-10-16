001679841fce27b17d2099589e02c3e7f9794c9a
hymbz <hymbz3516@gmail.com>
2020-10-16 21:22:28 +0800

    修复 dmzj 改回旧域名后导致的失效问题

--- ComicReadRaw.js
+++ ComicReadRaw.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name      ComicRead
-// @version     4.2
+// @version     4.3
 // @author      hymbz
 // @description 为漫画站增加双页阅读模式并优化使用体验。百合会——「记录阅读历史，体验优化」、动漫之家——「看被封漫画，导出导入漫画订阅/历史记录」、ehentai——「匹配 nhentai 漫画、Tag」、nhentai——「彻底屏蔽漫画，自动翻页」、dm5、manhuagui、manhuadb、mangabz、lhscan。部分支持站点以外的网站，也可以使用简易阅读模式来双页阅读漫画。
 // @namespace   ComicRead
@@ -476,9 +476,9 @@ switch (location.hostname) {
     '@@NewYamiboScript.@@';
     break;
   }
-  case 'i.dmzj1.com':
-  case 'm.dmzj1.com':
-  case 'manhua.dmzj1.com': {
+  case 'i.dmzj.com':
+  case 'm.dmzj.com':
+  case 'manhua.dmzj.com': {
     '@@DMZJScript.@@';
     break;
   }
