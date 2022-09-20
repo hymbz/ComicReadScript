3e43ab5a5ebe8c9da2fe6e7e20eb13df1f11ca3f
hymbz <hymbz3516@gmail.com>
2022-09-20 16:04:00 +0800

    fix: :bug: 修复 shadom 内无法应用 css 变量的 bug

--- packages/ui-component/src/containers/Manga/components/CssVar.tsx
+++ packages/ui-component/src/containers/Manga/components/CssVar.tsx
@@ -1,12 +1,13 @@
 import { memo } from 'react';
 import { useStore } from '../hooks/useStore';
+import classes from '../index.module.css';
 
 export const CssVar: React.FC = memo(() => {
   const cssVar = useStore((state) => state.cssVar);
 
   return (
     <style type="text/css">{`
-    :root {
+    .${classes.root} {
       ${Object.entries(cssVar)
         .map(([key, value]) => `--${key}: ${value};`)
         .join('\n')}
