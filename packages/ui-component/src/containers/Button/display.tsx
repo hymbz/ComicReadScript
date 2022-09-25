413fd2dd2c700737a2403444d1e0ab5824f83e36
hymbz <hymbz3516@gmail.com>
2022-09-25 21:55:35 +0800

    feat: :sparkles: 简单完成阅读按钮的样式

---
+++ packages/ui-component/src/containers/Button/display.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable react/function-component-definition */
+/*
+ * 用于测试时显示组件
+ */
+import { Button } from '.';
+
+// eslint-disable-next-line import/no-extraneous-dependencies
+import 'normalize.css';
+
+export default function DisplayManga() {
+  return <Button />;
+}
--- packages/ui-component/src/containers/Button/index.tsx
+++ packages/ui-component/src/containers/Button/index.tsx
@@ -1,14 +1,10 @@
 import classes from './index.module.css';
 
-const a = [1, 2];
-const b = [...a, 3];
-
 /** 测试按钮 */
 export const Button: React.FC = () => {
   return (
-    <>
-      <div className={classes.red}>Button</div>
-      <div>{b}</div>
-    </>
+    <button className={classes.button} type="button">
+      B
+    </button>
   );
 };
