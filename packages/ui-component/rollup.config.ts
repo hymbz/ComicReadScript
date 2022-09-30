d6473901758fff58a2a6e0e1e7f9134906a21506
hymbz <hymbz3516@gmail.com>
2022-09-30 14:37:03 +0800

    perf: :zap: 将 UI 组件改为懒加载执行

--- packages/ui-component/rollup.config.ts
+++ packages/ui-component/rollup.config.ts
@@ -1,7 +1,7 @@
 /* eslint-disable no-restricted-syntax */
 /* eslint-disable import/no-extraneous-dependencies */
 import fs from 'fs';
-import type { RollupOptions } from 'rollup';
+import type { RollupOptions, Plugin } from 'rollup';
 import ts from 'rollup-plugin-ts';
 import postcss from 'rollup-plugin-postcss';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
@@ -19,7 +19,37 @@ const buildConfig = (config: RollupOptions): RollupOptions => ({
   ],
   output: {
     dir: 'dist',
+    format: 'cjs',
     generatedCode: 'es2015',
+    strict: false,
+    plugins: [
+      // 将组件改为懒加载
+      {
+        name: 'lazyload',
+        renderChunk(code) {
+          return `${code.replace('\n\n', '\n\nconst main = () => {\n\n')}
+
+}
+
+const selfModule = module.exports;
+module.exports = new Proxy(selfModule, {
+  get(_, prop) {
+    if (selfModule[prop] === undefined) main();
+    return selfModule[prop];
+  },
+  apply(_, __, args) {
+    if (selfModule[prop] === undefined) main();
+    return selfModule[prop](...args);
+  },
+  construct(_, args) {
+    if (selfModule[prop] === undefined) main();
+    return new selfModule[prop](...args);
+  },
+});
+`;
+        },
+      } as Plugin,
+    ],
   },
   external: [...Object.keys(meta.resource ?? {})],
 
