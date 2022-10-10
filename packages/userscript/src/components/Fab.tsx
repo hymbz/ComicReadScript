ff6b5869c874ca5818408bb96adc5785fac60b35
hymbz <hymbz3516@gmail.com>
2022-10-10 18:10:32 +0800

    refactor: :recycle: 修改 showFab 的参数

--- packages/userscript/src/components/Fab.tsx
+++ packages/userscript/src/components/Fab.tsx
@@ -1,12 +1,12 @@
+import type { FabProps } from '@crs/ui-component/dist/Fab';
 import { Fab } from '@crs/ui-component/dist/Fab';
 import FabStyle from '@crs/ui-component/dist/Fab.css';
 import type ReactDOM from 'react-dom/client';
 import shadow from 'react-shadow';
-import type { MouseEventHandler } from 'react';
 import { querySelector } from '../helper';
 
 let FabRoot: ReactDOM.Root | null = null;
-export const showFab = (onClick: MouseEventHandler) => {
+export const showFab = (props: FabProps) => {
   // eslint-disable-next-line @typescript-eslint/consistent-type-imports
   const ReactDOM: typeof import('react-dom/client') = require('react-dom');
 
@@ -20,7 +20,7 @@ export const showFab = (onClick: MouseEventHandler) => {
   if (!FabRoot) FabRoot = ReactDOM.createRoot(dom);
   FabRoot.render(
     <shadow.div>
-      <Fab onClick={onClick} />
+      <Fab {...props} />
       <style type="text/css">{FabStyle}</style>
     </shadow.div>,
   );
--- packages/userscript/src/site/dmzj.tsx
+++ packages/userscript/src/site/dmzj.tsx
@@ -1,10 +1,12 @@
 import { showComicReadWindow } from '../components/ComicReadWindow';
 import { showFab } from '../components/Fab';
 
-showFab(() => {
-  showComicReadWindow(
-    [...document.querySelectorAll('.inner_img img')].map(
-      (i) => i.getAttribute('data-original')!,
-    ),
-  );
+showFab({
+  onClick: () => {
+    showComicReadWindow(
+      [...document.querySelectorAll('.inner_img img')].map(
+        (i) => i.getAttribute('data-original')!,
+      ),
+    );
+  },
 });
