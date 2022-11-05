79681ee92d1f9d511c210801b39105dcaf3b7bf8
hymbz <hymbz3516@gmail.com>
2022-11-05 22:13:39 +0800

    feat: :sparkles: 完成 ehentai 的 匹配nhentai漫画 功能

--- packages/userscript/src/site/ehentai.tsx
+++ packages/userscript/src/site/ehentai.tsx
@@ -4,9 +4,9 @@ import { querySelector } from '../helper';
 import { useInit } from '../helper/useInit';
 
 (async () => {
-  const { options, showFab, request, createShowComic } = await useInit(
+  const { options, showFab, request, createShowComic, toast } = await useInit(
     'nhentai',
-    { 自动翻页: true, 彻底屏蔽漫画: true, 在新页面中打开链接: true },
+    { 匹配nhentai漫画: true },
   );
 
   // 只在漫画页内运行
@@ -70,4 +70,52 @@ import { useInit } from '../helper/useInit';
   showFab({ progress: undefined, onClick: showComic });
 
   if (options.autoLoad) await showComic();
+
+  if (options.匹配nhentai漫画) {
+    const titleDom = document.getElementById('gn');
+    const taglistDom = querySelector('#taglist tbody');
+    if (!titleDom || !taglistDom) {
+      toast('页面结构发生改变，匹配 nhentai 漫画功能无法正常生效', {
+        type: 'error',
+      });
+      return;
+    }
+
+    const res = await request(
+      'GET',
+      `https://nhentai.net/api/gallerises/search?query=${encodeURIComponent(
+        titleDom.innerText,
+      )}`,
+      { errorText: 'nhentai 漫画匹配失败。可能是 nhentai 登录状态失效？' },
+    );
+
+    const nHentaiComicInfo = JSON.parse(res.responseText);
+
+    // 构建新标签行
+    const newTagLine = document.createElement('tr');
+    if (nHentaiComicInfo.result.length) {
+      let temp = '<td class="tc">nhentai:</td><td>';
+      let i = nHentaiComicInfo.result.length;
+      while (i) {
+        i -= 1;
+        const tempComicInfo = nHentaiComicInfo.result[i];
+        temp += `<div id="td_nhentai:${
+          tempComicInfo.id
+        }" class="gtl" style="opacity:1.0" title="${
+          tempComicInfo.title.japanese
+            ? tempComicInfo.title.japanese
+            : tempComicInfo.title.english
+        }"><a href="https://nhentai.net/g/${
+          tempComicInfo.id
+        }/" index=${i} onclick="return toggle_tagmenu('nhentai:${
+          tempComicInfo.id
+        }',this)">${tempComicInfo.id}</a></a></div>`;
+      }
+      newTagLine.innerHTML = `${temp}</td>`;
+    } else
+      newTagLine.innerHTML =
+        '<td class="tc">nhentai:</td><td class="tc" style="text-align: left;">Null</td>';
+
+    taglistDom.appendChild(newTagLine);
+  }
 })();
