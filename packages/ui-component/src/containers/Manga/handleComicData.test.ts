fa10b23e5d33115196636d5aadfae374a8f295f3
hymbz <hymbz3516@gmail.com>
2023-02-07 16:25:05 +0800

    test: :white_check_mark: 增加测试

---
+++ packages/ui-component/src/containers/Manga/handleComicData.test.ts
@@ -0,0 +1,91 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { describe, expect, it } from 'vitest';
+import { handleComicData } from './handleComicData';
+import type { FillEffect } from './hooks/useStore/ImageSlice';
+
+const mock = (
+  imgTypeList: ComicImg['type'][],
+  fillEffect: FillEffect = new Map(),
+) => ({
+  pageList: handleComicData({
+    comicImgList: imgTypeList.map(
+      (type, index): ComicImg => ({
+        type,
+        index,
+        loadType: 'loaded',
+        src: '',
+      }),
+    ),
+    fillEffect,
+  }),
+  fillEffect,
+});
+
+describe('跨页相关', () => {
+  it('跨页在开头', () => {
+    const { pageList, fillEffect } = mock(['long', '', '', '']);
+    expect(pageList).toMatchObject([[0], [1, 2], [3, -1]]);
+    expect(fillEffect).toMatchObject(new Map([[0, false]]));
+  });
+
+  it('跨页在中间，且会导致缺页', () => {
+    const { pageList, fillEffect } = mock(['', 'long', '']);
+    expect(pageList).toMatchObject([[-1, 0], [1], [2, -1]]);
+    expect(fillEffect).toMatchObject(new Map([[1, false]]));
+  });
+
+  it('跨页在结尾，且会导致缺页', () => {
+    const { pageList, fillEffect } = mock(['', '', '', 'long']);
+    expect(pageList).toMatchObject([[0, 1], [-1, 2], [3]]);
+    expect(fillEffect).toMatchObject(new Map([[3, false]]));
+  });
+
+  it('跨页在开头和结尾，且会导致缺页', () => {
+    const { pageList, fillEffect } = mock(['long', '', '', '', 'long']);
+    expect(pageList).toMatchObject([[0], [1, 2], [-1, 3], [4]]);
+    expect(fillEffect).toMatchObject(
+      new Map([
+        [0, false],
+        [4, false],
+      ]),
+    );
+  });
+
+  it('跨页在开头和中间，且会导致缺页', () => {
+    const { pageList, fillEffect } = mock(['long', '', '', '', 'long', '']);
+    expect(pageList).toMatchObject([[0], [1, 2], [-1, 3], [4], [5, -1]]);
+    expect(fillEffect).toMatchObject(
+      new Map([
+        [0, false],
+        [4, false],
+      ]),
+    );
+  });
+
+  it('跨页在中间和结尾，且会导致缺页', () => {
+    const { pageList, fillEffect } = mock(['', 'long', '', '', '', 'long']);
+    expect(pageList).toMatchObject([[-1, 0], [1], [2, 3], [-1, 4], [5]]);
+    expect(fillEffect).toMatchObject(
+      new Map([
+        [1, false],
+        [5, false],
+      ]),
+    );
+  });
+});
+
+describe('填充页相关', () => {
+  it('开头跨页，且要求开头填充', () => {
+    const { pageList, fillEffect } = mock(
+      ['long', '', '', ''],
+      new Map([[-1, true]]),
+    );
+    expect(pageList).toMatchObject([[0], [1, 2], [3, -1]]);
+    expect(fillEffect).toMatchObject(
+      new Map([
+        [-1, true],
+        [0, false],
+      ]),
+    );
+  });
+});
--- packages/ui-component/src/containers/Manga/handleComicData.ts
+++ packages/ui-component/src/containers/Manga/handleComicData.ts
@@ -23,19 +23,19 @@ export const handleComicData = ({
     if (fillEffect.get(i - 1)) {
       // FIXME: 按理来说应该是不会触发的，一般也确实不会触发，一旦触发 imgCache 就会被覆盖
       // 但确实有过在不知怎么操作后触发了的情况，所以在找到稳定复现的方法并解决前姑且先留着报错
-      if (imgCache) throw new Error('imgCache 被覆盖');
+      if (imgCache !== null) throw new Error('imgCache 被覆盖');
       imgCache = -1;
     }
 
     if (img.type !== 'long' && img.type !== 'wide') {
-      if (imgCache) {
+      if (imgCache !== null) {
         pageList.push([imgCache, img.index]);
         imgCache = null;
       } else {
         imgCache = img.index;
       }
     } else {
-      if (imgCache) {
+      if (imgCache !== null) {
         if (imgCache !== -1) pageList.push([-1, imgCache]);
         imgCache = null;
       }
@@ -47,7 +47,7 @@ export const handleComicData = ({
     }
   }
 
-  if (imgCache && imgCache !== -1) {
+  if (imgCache !== null && imgCache !== -1) {
     pageList.push([imgCache, -1]);
     imgCache = null;
   }
