8fb131e038e6391c38f7a6621e5a999aa0222dd2
hymbz <hymbz3516@gmail.com>
2023-02-12 01:12:47 +0800

    refactor: :recycle: 删除漫画图片容器

--- packages/ui-component/src/containers/Manga/components/ComicImg.tsx
+++ packages/ui-component/src/containers/Manga/components/ComicImg.tsx
@@ -1,5 +1,3 @@
-import type { ClassValue } from 'clsx';
-import clsx from 'clsx';
 import type { Draft } from 'immer';
 import { current } from 'immer';
 import type { SyntheticEvent } from 'react';
@@ -8,93 +6,77 @@ import { shallow, useStore } from '../hooks/useStore';
 
 import classes from '../index.module.css';
 
-export interface ComicImgProps {
-  index: number;
-  className?: ClassValue;
-}
-
 /**
  * 漫画图片
  *
  * @param img 图片数据
  */
-export const ComicImg: React.FC<ComicImgProps> = memo(
-  ({ index, className }) => {
-    const imgRef = useRef<HTMLImageElement>(null);
+export const ComicImg: React.FC<{ index: number }> = memo(({ index }) => {
+  const imgRef = useRef<HTMLImageElement>(null);
 
-    const handleImgLoaded = useCallback(() => {
+  const handleImgLoaded = useCallback(() => {
+    if (index === -1) return;
+    useStore.setState((state) => {
+      if (!imgRef.current) return;
+
+      const img = state.imgList[index];
+      img.loadType = 'loaded';
+      img.height = imgRef.current.naturalHeight;
+      img.width = imgRef.current.naturalWidth;
+      state.img.updateImgType(img);
+
+      // eslint-disable-next-line @typescript-eslint/no-floating-promises
+      state.onLoading?.(current(img), current(state.imgList));
+    });
+  }, [index]);
+
+  const handleImgError = useCallback(
+    (e: SyntheticEvent<HTMLImageElement, Event>) => {
       if (index === -1) return;
+      // 跳过因为 src 为空导致的错误
+      if ((e.target as HTMLImageElement).getAttribute('src') === '') return;
       useStore.setState((state) => {
-        if (!imgRef.current) return;
-
         const img = state.imgList[index];
-        img.loadType = 'loaded';
-        img.height = imgRef.current.naturalHeight;
-        img.width = imgRef.current.naturalWidth;
-        state.img.updateImgType(img);
+        img.loadType = 'error';
+        img.error = e as Draft<SyntheticEvent<HTMLImageElement, Event>>;
 
         // eslint-disable-next-line @typescript-eslint/no-floating-promises
         state.onLoading?.(current(img), current(state.imgList));
       });
-    }, [index]);
+    },
+    [index],
+  );
 
-    const handleImgError = useCallback(
-      (e: SyntheticEvent<HTMLImageElement, Event>) => {
-        if (index === -1) return;
-        // 跳过因为 src 为空导致的错误
-        if ((e.target as HTMLImageElement).getAttribute('src') === '') return;
-        useStore.setState((state) => {
-          const img = state.imgList[index];
-          img.loadType = 'error';
-          img.error = e as Draft<SyntheticEvent<HTMLImageElement, Event>>;
+  const { show, fill } = useStore((state) => {
+    // 卷轴模式下全部显示
+    if (state.option.scrollMode) return { show: '' };
 
-          // eslint-disable-next-line @typescript-eslint/no-floating-promises
-          state.onLoading?.(current(img), current(state.imgList));
-        });
-      },
-      [index],
-    );
+    const activePage = state.pageList[state.activePageIndex];
+    if (!activePage.includes(index)) return { show: undefined };
+    return {
+      show: '',
+      fill: ((): undefined | 'left' | 'right' => {
+        const i = activePage.indexOf(-1);
+        if (i === -1) return undefined;
+        return !!i === (state.option.dir === 'rtl') ? 'left' : 'right';
+      })(),
+    };
+  }, shallow);
 
-    const { show, fill } = useStore((state) => {
-      // 卷轴模式下全部显示
-      if (state.option.scrollMode) return { show: true };
+  const img = useStore((state) => state.imgList[index]);
 
-      const activePage = state.pageList[state.activePageIndex];
-      if (!activePage.includes(index)) return { show: false };
-      return {
-        show: true,
-        fill: ((): undefined | 'left' | 'right' => {
-          const i = activePage.indexOf(-1);
-          if (i === -1) return undefined;
-          return !!i === (state.option.dir === 'rtl') ? 'left' : 'right';
-        })(),
-      };
-    }, shallow);
-
-    const img = useStore((state) => state.imgList[index]);
-
-    return (
-      <div
-        className={classes.comicImg}
-        data-type={img.type}
-        data-fill={fill}
-        style={{ display: show ? undefined : 'none' }}
-      >
-        <img
-          className={clsx(
-            classes.img,
-            classes[img.type],
-            classes[img.loadType],
-            className,
-          )}
-          ref={imgRef}
-          src={img.loadType === 'wait' ? '' : img.src}
-          data-type={img.loadType}
-          alt={`${index}`}
-          onLoad={handleImgLoaded}
-          onError={handleImgError}
-        />
-      </div>
-    );
-  },
-);
+  return (
+    <img
+      ref={imgRef}
+      className={classes.img}
+      src={img.loadType === 'wait' ? '' : img.src}
+      alt={`${index}`}
+      data-show={show}
+      data-type={img.type}
+      data-load-type={img.loadType}
+      data-fill={fill}
+      onLoad={handleImgLoaded}
+      onError={handleImgError}
+    />
+  );
+});
