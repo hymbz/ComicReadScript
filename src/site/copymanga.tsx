cb734353a9ae39f2f9c069a9bf0596ce9b45c300
hymbz <hymbz3516@gmail.com>
2024-05-09 18:57:06 +0800

    fix: :bug: 修复拷贝漫画加载缓慢的问题

--- src/site/copymanga.tsx
+++ src/site/copymanga.tsx
@@ -1,50 +1,39 @@
 import {
   type InitOptions,
-  type RequestDetails,
-  eachApi,
   querySelectorClick,
   wait,
   querySelector,
   createStyle,
+  request,
 } from 'main';
 
-const apiList = [
-  'https://api.copymanga.info',
-  'https://api.copymanga.net',
-  'https://api.copymanga.org',
-  'https://api.copymanga.tv',
-  'https://api.xsskc.com',
-  'https://api.mangacopy.com',
-  'https://api.copymanga.site',
-];
-
-// API 参考：https://github.com/fumiama/copymanga/blob/279e08b06a70307bf20162900103ec1fdcb97751/app/src/res/values/strings.xml
+// API 参考：https://github.com/fumiama/copymanga/blob/279e08b06a70307bf20162900103ec1fdcb97751/app/src/main/res/values/strings.xml
 
 declare let options: InitOptions | undefined;
 
-function api<T = any>(url: string, details?: RequestDetails) {
-  return eachApi<T>(url, apiList, details);
-}
-
 (() => {
+  const headers = {
+    webp: '1',
+    region: '1',
+    'User-Agent': 'COPY/2.0.7|',
+    version: '2.0.7',
+    source: 'copyApp',
+    referer: 'com.copymanga.app-2.0.7',
+  };
   const token = document.cookie
     .split('; ')
     .find((cookie) => cookie.startsWith('token='))
     ?.replace('token=', '');
+  if (token) Reflect.set(headers, 'Authorization', `Token ${token}`);
 
-  if (window.location.href.includes('/chapter/')) {
-    const [, , name, , id] = window.location.pathname.split('/');
+  let name = '';
+  let id = '';
+  if (window.location.href.includes('/chapter/'))
+    [, , name, , id] = window.location.pathname.split('/');
+  else if (window.location.href.includes('/comicContent/'))
+    [, , , name, id] = window.location.pathname.split('/');
 
-    const headers = {
-      webp: '1',
-      region: '1',
-      'User-Agent': 'COPY/2.0.7|',
-      version: '2.0.7',
-      source: 'copyApp',
-      referer: 'com.copymanga.app-2.0.7',
-    };
-    if (token) Reflect.set(headers, 'Authorization', `Token ${token}`);
-
+  if (name && id) {
     const getImgList = async () => {
       type ResData = {
         message: string;
@@ -55,14 +44,13 @@ function api<T = any>(url: string, details?: RequestDetails) {
           };
         };
       };
-      const res = await api<ResData>(
-        `/api/v3/comic/${name}/chapter2/${id}?platform=3`,
-        { responseType: 'json', headers },
+      const res = await request<ResData>(
+        `${window.location.origin}/api/v3/comic/${name}/chapter2/${id}?platform=3`,
+        { responseType: 'json', headers, fetch: false },
       );
-      const data = res.response;
 
       const imgList: string[] = [];
-      const { words, contents } = data.results.chapter;
+      const { words, contents } = res.response.results.chapter;
       for (let i = 0; i < contents.length; i++)
         imgList[words[i]] = contents[i].url.replace('.c800x.', '.c1500x.');
       return imgList;
@@ -77,7 +65,7 @@ function api<T = any>(url: string, details?: RequestDetails) {
       ),
       async getCommentList() {
         const chapter_id = window.location.pathname.split('/').at(-1);
-        const res = await api(
+        const res = await request(
           `/api/v3/roasts?chapter_id=${chapter_id}&limit=100&offset=0&_update=true`,
           { responseType: 'json', errorText: '获取漫画评论失败' },
         );
@@ -115,16 +103,18 @@ function api<T = any>(url: string, details?: RequestDetails) {
 
       a.textContent = '獲取中';
       a.removeAttribute('href');
-      const res = await api(`/api/v3/comic2/${comicName}/query?platform=3`, {
-        headers: { Authorization: `Token ${token}` },
-      });
-      const data = JSON.parse(res.response);
-      if (data?.results?.browse === null) {
-        a.textContent = '無';
+      const res = await request(
+        `${window.location.origin}/api/v3/comic2/${comicName}/query?platform=3`,
+        { responseType: 'json', fetch: false, headers },
+      );
+
+      const data = res.response?.results?.browse;
+      if (!data) {
+        a.textContent = data === null ? '無' : '未返回數據';
         return;
       }
 
-      const lastChapterId = data?.results?.browse?.chapter_id as string;
+      const lastChapterId = data.chapter_id as string;
       if (!lastChapterId) {
         a.textContent = '接口異常';
         return;
@@ -136,7 +126,7 @@ function api<T = any>(url: string, details?: RequestDetails) {
       }`);
 
       a.href = `${window.location.pathname}/chapter/${lastChapterId}`;
-      a.textContent = data?.results?.browse?.chapter_name as string;
+      a.textContent = data.chapter_name as string;
     };
 
     setTimeout(updateLastChapter);
