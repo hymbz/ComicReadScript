fb21d96dc664f9b07ae7fa522b1b7322e9c6de6d
hymbz <hymbz3516@gmail.com>
2024-08-16 20:46:25 +0800

    fix: :bug: 修复 EmptyTip 未正确隐藏的 bug

--- src/components/Manga/components/EmptyTip.tsx
+++ src/components/Manga/components/EmptyTip.tsx
@@ -1,15 +1,28 @@
-import { type Component, onMount, onCleanup } from 'solid-js';
+import { onAutoMount } from 'helper';
+import { type Component } from 'solid-js';
 
 export const EmptyTip: Component = () => {
   let ref: HTMLHeadingElement;
 
-  onMount(() => {
-    let timeoutId = window.setTimeout(() => {
-      ref?.style.removeProperty('display');
-      timeoutId = 0;
-    }, 2000);
-    onCleanup(() => timeoutId && clearTimeout(timeoutId));
+  onAutoMount(() => {
+    let timeoutId = 0;
+    const observer = new IntersectionObserver(
+      ([{ isIntersecting }]) => {
+        if (!isIntersecting) return;
+        timeoutId = window.setTimeout(() => {
+          ref?.style.removeProperty('opacity');
+          timeoutId = 0;
+        }, 2000);
+      },
+      { threshold: 1 },
+    );
+    observer.observe(ref);
+
+    return () => {
+      observer.disconnect();
+      if (timeoutId) clearTimeout(timeoutId);
+    };
   });
 
-  return <h1 ref={ref!} style={{ display: 'none' }} textContent="NULL" />;
+  return <h1 ref={ref!} style={{ opacity: 0 }} textContent="NULL" />;
 };
--- src/components/Manga/store/other.ts
+++ src/components/Manga/store/other.ts
@@ -1,7 +1,4 @@
 export const otherState = {
-  /** 监视图片是否出现的 observer */
-  observer: null as IntersectionObserver | null,
-
   rootSize: { width: 0, height: 0 },
   scrollbarSize: { width: 0, height: 0 },
 };
