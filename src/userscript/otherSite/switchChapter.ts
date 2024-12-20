5d2706a51f92a1ccaae18632dbd0381f27aef9b9
hymbz <hymbz3516@gmail.com>
2024-12-20 17:51:16 +0800

    feat: 增加简易阅读模式能匹配到的上/下话切换按钮

--- src/userscript/otherSite/switchChapter.ts
+++ src/userscript/otherSite/switchChapter.ts
@@ -3,9 +3,8 @@ import { querySelectorAll } from 'helper';
 import type { MangaProps } from '../../components/Manga';
 import type { useInit } from '../main';
 
-const prevRe = /^(上一((章|章节|話|话))|prev chapter|前の章)$/i;
-
-const nextRe = /^(下一((章|章节|話|话))|next chapter|次の章)$/i;
+const prevRe = /^(上一?((章|章节|話|话))|prev|prev chapter|前の章)$/i;
+const nextRe = /^(下一?((章|章节|話|话))|next|next chapter|次の章)$/i;
 
 export const handleSwitchChapter = (
   setManga: AsyncReturnType<typeof useInit>['setManga'],
@@ -14,7 +13,10 @@ export const handleSwitchChapter = (
   let onNext: MangaProps['onNext'];
 
   for (const element of querySelectorAll('a, button')) {
-    const text = element.textContent?.trim();
+    // 删除可能混在其中的特殊符号
+    const text = element.textContent
+      ?.replaceAll(/[<>()《》（）「」『』]/g, '')
+      .trim();
     if (!text) continue;
     if (!onPrev && prevRe.test(text)) onPrev = () => element.click();
     if (!onNext && nextRe.test(text)) onNext = () => element.click();
