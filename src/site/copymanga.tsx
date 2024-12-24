e69ffca2fa629a71a56977af8b0ed19acfd12656
hymbz <hymbz3516@gmail.com>
2024-12-24 22:45:08 +0800

    fix: :bug: 修复拷贝漫画部分隐藏漫画未生成目录的 bug

--- src/site/copymanga.tsx
+++ src/site/copymanga.tsx
@@ -76,11 +76,16 @@ const handleLastChapter = (comicName: string) => {
   document.addEventListener('visibilitychange', updateLastChapter);
 };
 
-// 生成目录
-const buildChapters = async (comicName: string, isMobile: boolean) => {
-  // 拷贝有些漫画虽然可以通过 api 获取到数据，但网页上的目录被隐藏了
-  // 举例：https://mangacopy.com/comic/yueguangxiadeyishijiezhilv
+type HiddenType = 'web' | 'mobile' | '404';
 
+// 拷贝有些漫画虽然可以通过 api 获取到数据，但网页上的目录被隐藏了
+//  web - https://www.mangacopy.com/comic/lianyuqingchang
+//  mobile - https://www.mangacopy.com/h5/details/comic/lianyuqingchang
+// 还有些漫画连网页端介绍都被删了
+//  404 - https://www.mangacopy.com/comic/Hyakkasou
+
+// 生成目录
+const buildChapters = async (comicName: string, hiddenType: HiddenType) => {
   const {
     response: { results },
   } = await request<{ results: string }>(`/comicdetail/${comicName}/chapters`, {
@@ -140,148 +145,202 @@ const buildChapters = async (comicName: string, isMobile: boolean) => {
       Object.fromEntries(type.map(({ id }) => [id, []]));
     for (const chapter of props.chapters) chapters[chapter.type].push(chapter);
 
-    if (isMobile) {
-      // 删掉占位置的分隔线
-      for (const dom of querySelectorAll('.van-divider')) dom.remove();
+    switch (hiddenType) {
+      case 'mobile': {
+        // 删掉占位置的分隔线
+        for (const dom of querySelectorAll('.van-divider')) dom.remove();
 
-      return (
-        <div class="detailsTextContentTabs van-tabs van-tabs--line">
+        return (
+          <div class="detailsTextContentTabs van-tabs van-tabs--line">
+            <For each={type}>
+              {({ id, name }) => (
+                <Show when={chapters[id].length}>
+                  <div class="van-tabs__wrap">
+                    <div
+                      role="tablist"
+                      class="van-tabs__nav van-tabs__nav--line"
+                      style={{ background: 'transparent' }}
+                    >
+                      <div role="tab" class="van-tab van-tab--active">
+                        <span class="van-tab__text van-tab__text--ellipsis">
+                          <span>{name}</span>
+                        </span>
+                      </div>
+                      <div
+                        class="van-tabs__line"
+                        style={{
+                          width: '0.24rem',
+                          transform: 'translateX(187.5px) translateX(-50%)',
+                          'transition-duration': '0.3s',
+                        }}
+                      />
+                    </div>
+                  </div>
+                  <div class="van-tab__pane">
+                    <div
+                      class="chapterList van-grid"
+                      style={{ 'padding-left': '0.24rem' }}
+                    >
+                      <For each={chapters[id]}>
+                        {(chapter) => (
+                          <div
+                            class="chapterItem oneLines van-grid-item"
+                            classList={{
+                              red: props.last_chapter.uuid === chapter.id,
+                            }}
+                            style={{
+                              'flex-basis': '25%',
+                              'padding-right': '0.24rem',
+                              'margin-top': '0.24rem',
+                            }}
+                          >
+                            <a
+                              class="van-grid-item__content van-grid-item__content--center"
+                              href={`/comic/${comicName}/chapter/${chapter.id}`}
+                            >
+                              <span
+                                class="van-grid-item__text"
+                                children={chapter.name}
+                              />
+                            </a>
+                          </div>
+                        )}
+                      </For>
+                    </div>
+                  </div>
+                </Show>
+              )}
+            </For>
+          </div>
+        );
+      }
+
+      case 'web': {
+        return (
+          <>
+            <span>{props.name}</span>
+            <div class="table-default">
+              <div class="table-default-title">
+                <ul class="nav nav-tabs" role="tablist">
+                  <For each={type}>
+                    {({ id, name }) => (
+                      <li class="nav-item">
+                        <a
+                          class="nav-link"
+                          classList={{ disabled: chapters[id].length === 0 }}
+                          data-toggle="tab"
+                          href={`#${props.path_word}${name}`}
+                          role="tab"
+                          aria-selected="false"
+                          children={name}
+                        />
+                      </li>
+                    )}
+                  </For>
+                </ul>
+                <div class="table-default-right">
+                  <span>更新內容：</span>
+                  <a
+                    href={`/comic/${comicName}/chapter/${props.last_chapter.comic_id}`}
+                    target="_blank"
+                    children={props.last_chapter.name}
+                  />
+                  <span>更新時間：</span>
+                  <span>{props.last_chapter.datetime_created}</span>
+                </div>
+              </div>
+
+              <div class="table-default-box">
+                <div class="tab-content">
+                  <For each={type}>
+                    {({ id, name }) => (
+                      <div
+                        id={`${props.path_word}${name}`}
+                        role="tabpanel"
+                        class="tab-pane fade"
+                      >
+                        <ul>
+                          <For each={chapters[id]}>
+                            {(chapter) => (
+                              <a
+                                href={`/comic/${comicName}/chapter/${chapter.id}`}
+                                target="_blank"
+                                title={chapter.name}
+                                style={{ display: 'block' }}
+                              >
+                                <li>{chapter.name}</li>
+                              </a>
+                            )}
+                          </For>
+                        </ul>
+                      </div>
+                    )}
+                  </For>
+                </div>
+              </div>
+            </div>
+          </>
+        );
+      }
+
+      default: {
+        return (
           <For each={type}>
             {({ id, name }) => (
               <Show when={chapters[id].length}>
-                <div class="van-tabs__wrap">
-                  <div
-                    role="tablist"
-                    class="van-tabs__nav van-tabs__nav--line"
-                    style={{ background: 'transparent' }}
-                  >
-                    <div role="tab" class="van-tab van-tab--active">
-                      <span class="van-tab__text van-tab__text--ellipsis">
-                        <span>{name}</span>
-                      </span>
-                    </div>
-                    <div
-                      class="van-tabs__line"
-                      style={{
-                        width: '0.24rem',
-                        transform: 'translateX(187.5px) translateX(-50%)',
-                        'transition-duration': '0.3s',
-                      }}
-                    />
-                  </div>
-                </div>
-                <div class="van-tab__pane">
-                  <div
-                    class="chapterList van-grid"
-                    style={{ 'padding-left': '0.24rem' }}
-                  >
-                    <For each={chapters[id]}>
-                      {(chapter) => (
-                        <div
-                          class="chapterItem oneLines van-grid-item"
-                          classList={{
-                            red: props.last_chapter.uuid === chapter.id,
-                          }}
-                          style={{
-                            'flex-basis': '25%',
-                            'padding-right': '0.24rem',
-                            'margin-top': '0.24rem',
-                          }}
-                        >
+                <div
+                  class="card"
+                  style={{ 'max-width': '100em', margin: '1em auto' }}
+                >
+                  <div class="card-body">
+                    <h2 class="card-title">{name}</h2>
+
+                    <ul>
+                      <For each={chapters[id]}>
+                        {(chapter) => (
                           <a
-                            class="van-grid-item__content van-grid-item__content--center"
+                            class="btn btn-outline-primary"
+                            classList={{
+                              active: props.last_chapter.uuid === chapter.id,
+                            }}
                             href={`/comic/${comicName}/chapter/${chapter.id}`}
-                          >
-                            <span
-                              class="van-grid-item__text"
-                              children={chapter.name}
-                            />
-                          </a>
-                        </div>
-                      )}
-                    </For>
+                            children={chapter.name}
+                          />
+                        )}
+                      </For>
+                    </ul>
                   </div>
                 </div>
               </Show>
             )}
           </For>
-        </div>
-      );
+        );
+      }
     }
-
-    return (
-      <>
-        <span>{props.name}</span>
-        <div class="table-default">
-          <div class="table-default-title">
-            <ul class="nav nav-tabs" role="tablist">
-              <For each={type}>
-                {({ id, name }) => (
-                  <li class="nav-item">
-                    <a
-                      class="nav-link"
-                      classList={{ disabled: chapters[id].length === 0 }}
-                      data-toggle="tab"
-                      href={`#${props.path_word}${name}`}
-                      role="tab"
-                      aria-selected="false"
-                      children={name}
-                    />
-                  </li>
-                )}
-              </For>
-            </ul>
-            <div class="table-default-right">
-              <span>更新內容：</span>
-              <a
-                href={`/comic/${comicName}/chapter/${props.last_chapter.comic_id}`}
-                target="_blank"
-                children={props.last_chapter.name}
-              />
-              <span>更新時間：</span>
-              <span>{props.last_chapter.datetime_created}</span>
-            </div>
-          </div>
-
-          <div class="table-default-box">
-            <div class="tab-content">
-              <For each={type}>
-                {({ id, name }) => (
-                  <div
-                    id={`${props.path_word}${name}`}
-                    role="tabpanel"
-                    class="tab-pane fade"
-                  >
-                    <ul>
-                      <For each={chapters[id]}>
-                        {(chapter) => (
-                          <a
-                            href={`/comic/${comicName}/chapter/${chapter.id}`}
-                            target="_blank"
-                            title={chapter.name}
-                            style={{ display: 'block' }}
-                          >
-                            <li>{chapter.name}</li>
-                          </a>
-                        )}
-                      </For>
-                    </ul>
-                  </div>
-                )}
-              </For>
-            </div>
-          </div>
-        </div>
-      </>
-    );
   };
 
-  render(
-    () => <For each={Object.values(groups)} children={Group} />,
-    isMobile
-      ? querySelector('.detailsTextContent')!
-      : querySelector('.upLoop')!,
-  );
+  let root: HTMLElement;
+  switch (hiddenType) {
+    case 'mobile':
+      root = querySelector('.detailsTextContent')!;
+      // 自动点掉隐藏漫画的提示
+      for (const element of querySelectorAll('button.van-dialog__confirm'))
+        element.click();
+      break;
+    case 'web':
+      root = querySelector('.upLoop')!;
+      break;
+    default:
+      root = querySelector('main')!;
+      root.textContent = '';
+
+      GM_addStyle(
+        `ul .btn { height: fit-content; width: fit-content; margin: 1em; }`,
+      );
+      break;
+  }
+
+  render(() => <For each={Object.values(groups)} children={Group} />, root);
 
   // 点击每个分组下第一个激活的标签
   for (const group of querySelectorAll('.upLoop .table-default-title'))
@@ -331,8 +390,13 @@ const buildChapters = async (comicName: string, isMobile: boolean) => {
       };
       const res = await request<ResData>(
         `/api/v3/comic/${comicName}/chapter2/${id}?platform=3`,
-        { responseType: 'json', headers },
+        { responseType: 'json', headers, noCheckCode: true },
       );
+      if (res.status !== 200) {
+        const message = `漫畫加載失敗：${res.response.message || res.status}`;
+        if (titleDom) titleDom.textContent = message;
+        throw new Error(message);
+      }
       if (titleDom) {
         titleDom.textContent = '漫畫加載成功🥳';
         const {
@@ -389,43 +453,58 @@ const buildChapters = async (comicName: string, isMobile: boolean) => {
     comicName = window.location.href.split('/comic/')[1];
     if (!comicName) return;
 
-    let isHidden = false;
+    let hiddenType: HiddenType | undefined;
     const isMobile = window.location.href.includes('/h5/');
-    if (isMobile) {
+
+    if (document.title === '404 - 拷貝漫畫') {
+      // 移动端可以直接复用代码来实现相同的样式
+      hiddenType = isMobile ? 'mobile' : '404';
+    } else if (isMobile) {
       // 等到加载提示框消失
       await wait(
         () =>
           querySelector('.van-toast__text')?.parentElement?.style.display ===
           'none',
       );
-      // 再等一秒看有没有屏蔽提示
-      if (
-        await wait(
-          () => querySelector('.isBan')?.textContent?.includes('不提供閱覽'),
-          1000,
-        )
-      ) {
-        isHidden = true;
-      }
-    } else {
-      isHidden =
-        // 先检查有没有屏蔽提示
-        Boolean(
-          querySelector('.wargin')?.textContent?.includes('不提供閱覽'),
-        ) ||
-        // 再等一秒看目录有没有加载出来
-        !(await wait(
-          () => querySelector('.upLoop .table-default-title'),
-          1000,
-        ));
+      // 再等一会看有没有屏蔽提示
+      hiddenType = await wait<HiddenType | undefined>(() => {
+        // 正常隐藏
+        if (querySelector('.isBan')?.textContent?.includes('不提供閱覽'))
+          return 'mobile';
+        // 连介绍都没有的隐藏
+        const dialog = querySelector('.van-dialog__message');
+        if (dialog?.textContent?.includes('漫畫未找到')) {
+          dialog.textContent = '漫畫未找到!\n請坐和放寬，等待目錄生成';
+          // 删掉空白占位的原目录元素
+          for (const element of querySelectorAll('.detailsTextContentTabs'))
+            element.remove();
+          // 虽然实际是应该算是 404 类型，但因为网页的 css 还在
+          // 所以可以直接使用 mobile 的元素复用样式
+          return 'mobile';
+        }
+      }, 1000);
+    } else if (
+      // 先检查有没有屏蔽提示
+      Boolean(querySelector('.wargin')?.textContent?.includes('不提供閱覽')) ||
+      // 再等一秒看目录有没有加载出来
+      !(await wait(() => querySelector('.upLoop .table-default-title'), 1000))
+    ) {
+      // 检查漫画介绍是否正常显示
+      hiddenType = querySelector('.comicParticulars-title') ? 'web' : '404';
     }
 
     // 如果漫画被隐藏了，就自己生成目录
-    if (isHidden) {
+    if (hiddenType) {
       // 给屏蔽提示加个删除线
       const tip = querySelector('.isBan, .wargin');
       if (tip) tip.style.textDecoration = 'line-through';
-      await buildChapters(comicName, isMobile);
+      // 修改 404 提示
+      const titleDom = querySelector('main .img+.title');
+      if (titleDom) {
+        titleDom.textContent =
+          'ComicRead 提示您：你訪問的內容暫不存在，請坐和放寬，等待目錄生成';
+      }
+      await buildChapters(comicName, hiddenType);
     }
 
     if (!isMobile && token) handleLastChapter(comicName);
