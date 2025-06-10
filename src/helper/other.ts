242a2ade9bf9705d731588de9d84274a03f57c30
hymbz <hymbz3516@gmail.com>
2025-06-10 23:14:44 +0800

    fix: :bug: 修复部分情况下简易模式无法正常加载图片的 bug
    https://sleazyfork.org/scripts/374903/discussions/265139

--- src/helper/other.ts
+++ src/helper/other.ts
@@ -564,7 +564,7 @@ export const onUrlChange = async (
     if (!(await wait(() => handleUrl(window.location) !== lastUrl, 5000)))
       return;
     const nowUrl = handleUrl(window.location);
-    await fn(lastUrl, nowUrl);
+    if (lastUrl) await fn(lastUrl, nowUrl);
     lastUrl = nowUrl;
   });
 
