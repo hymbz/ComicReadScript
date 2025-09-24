f2a5f86f5aaaedd85697295d525b4f33ed00f7d6
hymbz <hymbz3516@gmail.com>
2025-09-25 02:07:00 +0800

    fix: :bug: 修复 PWA 无法点击按钮的 bug

--- src/pwa/src/option.ts
+++ src/pwa/src/option.ts
@@ -8,25 +8,14 @@ let saveOption: MangaProps['option'] | undefined;
 
 export const getSaveOption = (): MangaProps['option'] => {
   if (saveOption) return saveOption;
-  const saveVersion = localStorage.getItem('version') || '1.0.0';
-  const saveJson = localStorage.getItem('option');
+  const saveVersion = localStorage.getItem('@version') || '1.0.0';
+  const saveJson = localStorage.getItem('@option');
   if (!saveJson) return undefined;
   const option = JSON.parse(saveJson);
 
   if (saveVersion !== version) {
-    if (versionLt(version, '11.10')) {
-      if (typeof option?.scrollMode === 'boolean')
-        option.scrollMode = {
-          enabled: option.scrollMode,
-          spacing: option.scrollModeSpacing,
-          imgScale: option.scrollModeImgScale,
-          fitToWidth: option.scrollModeFitToWidth,
-        };
-      Reflect.deleteProperty(option, 'translation');
-    }
-
-    localStorage.setItem('version', version);
-    localStorage.setItem('option', JSON.stringify(option));
+    localStorage.setItem('@version', version);
+    if (versionLt(version, '12.3.3')) localStorage.removeItem('@option');
   }
 
   saveOption = option;
