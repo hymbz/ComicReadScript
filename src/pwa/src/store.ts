04ed88137d0e806910c55dcd617c7744b14b9acd
hymbz <hymbz3516@gmail.com>
2026-01-25 19:09:45 +0800

    fix: :bug: 修复 PWA 在打开嵌套文件夹时没有按照文件路径排序的 bug
    #309

--- src/pwa/src/store.ts
+++ src/pwa/src/store.ts
@@ -29,6 +29,14 @@ export const handleExit = () => setState('show', false);
 
 const collator = new Intl.Collator(undefined, { numeric: true });
 
+const sortFiles = (files: File[]) => {
+  // 我也不知道这个 path 是哪里来的，总之它就是存在
+  // 在 edge 上 webkitRelativePath 为空，但有 path。在火狐上，则是两个属性都有
+  const getFilePath = (file: File) =>
+    file.webkitRelativePath || (file as any).path || file.name;
+  files.sort((a, b) => collator.compare(getFilePath(a), getFilePath(b)));
+};
+
 /** 加载新的文件列表 */
 export const loadNewImglist = async (files: File[], errorTip?: string) => {
   if (files.length === 0) return;
@@ -36,6 +44,7 @@ export const loadNewImglist = async (files: File[], errorTip?: string) => {
   if (store.loading) return toast.warn(t('pwa.alert.repeat_load'));
 
   setState('loading', true);
+  sortFiles(files);
 
   try {
     const imgListRaw = await Promise.all(files.map(getImgData));
@@ -52,7 +61,6 @@ export const loadNewImglist = async (files: File[], errorTip?: string) => {
       state.imgList = [];
     });
     setState((state) => {
-      newImglist.sort((a, b) => collator.compare(a.name, b.name));
       state.imgList = newImglist;
       state.show = true;
 
