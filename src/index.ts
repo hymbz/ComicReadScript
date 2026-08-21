bc3d84a4e42505f685211c619ce58cddd8588d52
hymbz <hymbz3516@gmail.com>
2026-08-21 21:44:46 +0800

    fix: :bug: 修复在绅士漫画的 index 页进入阅读模式的 bug
    #359

--- src/index.ts
+++ src/index.ts
@@ -495,12 +495,6 @@ try {
     case 'siteUrl#wnacg':
     case 'www.wnacg.com':
     case 'wnacg.com': {
-      // 旧版：https://www.wnacg.com/photos-slist-aid-376223.html
-      // 新版：https://www.wnacg.com/photos-slide-aid-376223.html
-      // 条漫（？）：https://www.wnacg.com/photos-list-aid-376223.html
-      // 阅读器有多种类型，网址始终是 photos-<类型>-aid-<id> 的格式
-      // <类型> 仅影响阅读器，不影响资源的加载
-
       // 突出显示下拉阅读的按钮
       const buttonDom = querySelector('#bodywrap a.btn');
       if (buttonDom) {
@@ -508,26 +502,36 @@ try {
         buttonDom.style.setProperty('background-image', 'none');
       }
 
-      let getImgList: SetupOptions['getImgList'] | undefined;
-      if (unsafeWindow.imglist)
-        getImgList = () =>
-          (unsafeWindow.imglist as { url: string; caption: string }[])
-            .filter(
-              ({ caption }) => caption !== '喜歡紳士漫畫的同學請加入收藏哦！',
-            )
-            .map(({ url }) => url);
-      else if (location.pathname.includes('-aid-'))
-        getImgList = async () => {
-          const id = /aid-(?<id>\d+).html/u.exec(location.pathname)?.groups?.id;
-          if (!id) throw new Error(t('site.changed_load_failed'));
-          const res = await request<string>(`/photos-item-aid-${id}.html`);
-          const pageUrl = /"page_url":(?<pageUrl>\[.+\]),/u.exec(
-            res.responseText,
-          )?.groups.pageUrl;
-          if (!pageUrl) throw new Error(t('site.changed_load_failed'));
-          return eval(pageUrl) as string[]; // oxlint-disable-line no-eval
-        };
-      else break;
+      // 旧版：https://www.wnacg.com/photos-slist-aid-376223.html
+      // 新版：https://www.wnacg.com/photos-slide-aid-376223.html
+      // 条漫（？）：https://www.wnacg.com/photos-list-aid-376223.html
+      // 阅读器有多种类型，网址始终是 photos-<类型>-aid-<id> 的格式
+      // <类型> 仅影响阅读器，不影响资源的加载
+
+      const match = /\/photos-(?<type>slist|slide|list)-aid-(?<id>\d+)/u.exec(
+        location.pathname,
+      )?.groups;
+      if (!match?.type || !match?.id || match?.type === 'index') break;
+
+      const getImgList: SetupOptions['getImgList'] | undefined =
+        unsafeWindow.imglist
+          ? () =>
+              (unsafeWindow.imglist as { url: string; caption: string }[])
+                .filter(
+                  ({ caption }) =>
+                    caption !== '喜歡紳士漫畫的同學請加入收藏哦！',
+                )
+                .map(({ url }) => url)
+          : async () => {
+              const res = await request<string>(
+                `/photos-item-aid-${match.id}.html`,
+              );
+              const pageUrl = /"page_url":(?<pageUrl>\[.+\]),/u.exec(
+                res.responseText,
+              )?.groups.pageUrl;
+              if (!pageUrl) throw new Error(t('site.changed_load_failed'));
+              return eval(pageUrl) as string[]; // oxlint-disable-line no-eval
+            };
 
       setup({ name: 'wnacg', getImgList });
       break;
